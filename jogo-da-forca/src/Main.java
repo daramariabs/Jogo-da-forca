a7542bdb699466071205d7be01278b059bf92f18
daramariabs <darasousa09@gmail.com>
2022-01-23 12:02:55 -0300

    Menu

--- jogo-da-forca/src/Main.java
+++ jogo-da-forca/src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("funcionando");
+		Scanner sc = new Scanner(System.in);
+		menu();
+		
+		sc.close();
 	}
+	
+	public static void menu() {
+		System.out.println("----------------------");
+		System.out.println("MENU");
+		System.out.println("----------------------");
+		System.out.println("1. Gerenciar Temas");
+		System.out.println("2. Gerenciar Palavras");
+		System.out.println("3. Jogar");
+		System.out.println("4. Sair");
+		System.out.println("----------------------");
+		System.out.print("O que você deseja fazer?");
+    }
 
 }
