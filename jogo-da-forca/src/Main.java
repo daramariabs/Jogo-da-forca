8ff289ced36c723756ec6b030ef541dcd8c2297d
daramariabs <darasousa09@gmail.com>
2022-01-24 18:35:15 -0300

    Gerenciador de temas:finalizado.

--- jogo-da-forca/src/Main.java
+++ jogo-da-forca/src/Main.java
@@ -5,61 +5,59 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int escolhaMenu;
-		String[][] matrizJogo = new String[3][3];
+		String[][] matrizJogo = new String[50][50];
 		
-		matrizJogo[0][0] = "Cidade";
-		matrizJogo[0][1] = "Palmas";
-		matrizJogo[0][2] = "Paraiso";
-		matrizJogo[1][0] = "Frutas";
-		matrizJogo[1][1] = "Morango";
-		matrizJogo[1][2] = "Uva";
-		matrizJogo[2][0] = "Pais";
-		matrizJogo[2][1] = "Brasil";
-		matrizJogo[2][2] = "Mexico";
+		matrizJogo[0][0] = "cidade";
+		matrizJogo[0][1] = "palmas";
+		matrizJogo[0][2] = "paraiso";
+		matrizJogo[1][0] = "frutas";
+		matrizJogo[1][1] = "morango";
+		matrizJogo[1][2] = "uva";
+		matrizJogo[2][0] = "pais";
+		matrizJogo[2][1] = "brasil";
+		matrizJogo[2][2] = "mexico";
+		matrizJogo[3][0] = "cores";
 		
 		lerMatriz(matrizJogo);
-//		do {
-//			System.out.println("----------------------");
-//			System.out.println("MENU");
-//			System.out.println("----------------------");
-//			System.out.println("1. Gerenciar Temas");
-//			System.out.println("2. Gerenciar Palavras");
-//			System.out.println("3. Jogar");
-//			System.out.println("4. Sair");
-//			System.out.println("----------------------");
-//			System.out.print("O que você deseja fazer? ");
-//			escolhaMenu = sc.nextInt();
-//			
-//			if (escolhaMenu == 1) {
-//				gerenciarTemas();
-//			}
-//			else if(escolhaMenu == 2) {
-//				System.out.println("Gerenciar Palavras");
-//			}
-//			else if(escolhaMenu == 3) {
-//				System.out.println("Jogar");
-//			}
-//			
-//		}while(escolhaMenu != 4);
+		
+		System.out.println("----------------------");
+		System.out.println("MENU");
+		System.out.println("----------------------");
+		System.out.println("1. Gerenciar Temas");
+		System.out.println("2. Gerenciar Palavras");
+		System.out.println("3. Jogar");
+		System.out.println("4. Sair");
+		System.out.println("----------------------");
+		System.out.print("O que você deseja fazer? ");
+		escolhaMenu = sc.nextInt();
+		
+		if (escolhaMenu == 1) {
+			gerenciarTemas(matrizJogo);
+		}
+		
+
+		lerMatriz(matrizJogo);
 		
 		sc.close();	
 	}
 	
 	public static void lerMatriz(String[][] matriz) {
-		for(int i =0; i< 3; i++) {
-			for (int j= 0; j < 3; j++) {
+		for(int i =0; i< 10; i++) {
+			for (int j= 0; j < 10; j++) {
 				System.out.print(matriz[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
 	
-	public static void gerenciarTemas() {
+	public static void gerenciarTemas(String[][] matrizTemas) {
 		Scanner sc = new Scanner(System.in);
+		
 		int opcao;
 		int contPesquisa = 0;
-		String[][] matrizTemas = new String[3][3];
-		String temaExcluido, temaBuscado;
+		String novoTema = "";
+		String excluiTema = "";
+		String buscaTema = "";
 		
 		System.out.println("--------------------");
 		System.out.println("Grenciador de temas");
@@ -73,43 +71,58 @@ public class Main {
 		
 		while(opcao != 0) {
 			
-			switch(opcao) {
-			case 1:
-				// Falta implementar o não cadastro de palavras repetidas.
-				System.out.println("Informe os temas: ");
-				for (int i = 0; i < 3; i++) {
-					 	matrizTemas[i][0] = sc.nextLine().toLowerCase();
+			if(opcao == 1) {
+				//Cadastro de Temas
+				System.out.print("Informe o tema que deseja cadastrar: ");
+				novoTema = sc.nextLine().toLowerCase();
+				for(int i =0; i< 5; i++) {
+					if(novoTema.equals(matrizTemas[i][0])) {
+						System.out.println("O tema já existe. Impossivel cadastrar!");
+						break;
+					}
+					else if(matrizTemas[i][0] == null) {
+						matrizTemas[i][0] = novoTema;
+						System.out.println("O tema foi cadastrado.");
+						break;
+					}	
 				}
-				break;
-				
-			case 2:
-				//Falta finalizar a exclusão do tema
-				System.out.println("Temas cadastrados:");
-				for (int i = 0; i < 3; i++) {
-						System.out.println(matrizTemas[i][0].toLowerCase());
+			}
+			else if(opcao == 2) {
+				//Excluir Temas
+				System.out.print("Informe o tema que deseja Excluir: ");
+				excluiTema = sc.nextLine().toLowerCase();
+				for(int i = 0; i< 5; i++) {
+					if(excluiTema.equals(matrizTemas[i][0])) {
+						if(matrizTemas[i][1] == null) {
+							matrizTemas[i][0] = null;
+							System.out.println("Tema excluido...");
+						}
+						else {
+							System.out.println("Não foi possível excluir o tema. Verifique se existem palavras cadastradas nesse tema.");
+						}
+					}
+					
 				}
-				
-				System.out.println("Qual tema deseja excluir? ");
-				break;
-				
-			case 3:
+			}
+			else if(opcao == 3) {
+				//Buscar Temas
 				System.out.print("Informe o tema que deseja buscar: ");
-				temaBuscado = sc.nextLine();
+				buscaTema = sc.nextLine().toLowerCase();
 				
-				for (int i = 0; i < 3; i++) {
-					if (temaBuscado.equals(matrizTemas[i][0])) {
+				for (int i = 0; i < 5; i++) {
+					if (buscaTema.equals(matrizTemas[i][0])) {
+						System.out.println("Tema encontrado: " + matrizTemas[i][0]);
 						contPesquisa++;
+						break;
 					}
 				}
 				
-				if (contPesquisa == 1) {
-					System.out.println("Tema encontrado!");
-				}
-				else {
+				if(contPesquisa == 0) {
 					System.out.println("Tema não encontrado!");	
 				}
-				break;
+				
 			}
+			//Final dos ifs
 			
 			System.out.println("--------------------");
 			System.out.println("Grenciador de temas");
@@ -119,9 +132,13 @@ public class Main {
 			System.out.println("0. Sair ");
 			System.out.println("--------------------");
 			opcao = sc.nextInt();
-
+			sc.nextLine();
 			
 		}
+		
+		
+		
+		
 		
 		sc.close();
 	}
